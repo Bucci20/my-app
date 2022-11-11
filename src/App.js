848459fc0f83dbb379c8b97766eec1d65d2992cc
giorgi <100317242+Bucci20@users.noreply.github.com>
2022-11-11 23:47:37 +0400

    React-Workshop3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
+
+
+
+function generateUsers () {
+  return [
+    {name: 'Giorgi', id: 1},
+    {name: 'Qeti', id: 2},
+    {name: 'Salome', id: 3},
+    {name: 'Lasha', id: 4},
+    {name: 'mariami', id: 5},
+    {name: 'gaga', id: 6},
+    {name: 'Dato', id: 7},
+    {name: 'Matilda', id: 8},
+    {name: 'Giorgi', id: 9},
+    {name: 'Qeti', id: 10},
+    
+    
+  ];
+
+}  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [users, setUsers] = useState([]);
+  
+  useEffect(()=>{
+    setUsers(generateUsers())
+  },[])
 
+  useEffect(() => {
+    document.title = `${users.length} users left`;
+  });
+
+return (
+<div className='app'>
+<button onClick={() => 
+  setUsers((prevUsers) => prevUsers.slice(1))}>
+   Delete
+  </button>
+
+{users.map(({id, name}) => (
+  <h1 key={id}>{name}</h1>
+))}
+
+</div>
+);
+};
 export default App;
